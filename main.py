a5056c4bfb2525117cc7ae1dbd3ef63012598d5b
Jawa <joshuasingh076@gmail.com>
2023-01-04 02:08:21 -0500

    scrape gme stock price

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hi')
+import yfinance as yf
+
+tickers = ['GME']
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker)
+    data = ticker_yahoo.history()
+    last_quote = data['Close'].iloc[-1]
+    print(ticker, last_quote)
