4ab1b53959f07cb9d1998272606fff2f84299cb6
Jawa <joshuasingh076@gmail.com>
2023-01-16 22:56:48 -0500

    added plots reworked watchlist

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 import stonk
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import yfinance as yf
+import os
+
+
 
 with open('token.txt', 'r', encoding='utf-8') as f:
     bottoken = f.read()
@@ -9,7 +16,6 @@ TOKEN = bottoken
 CHANNEL = 1061108864898039818
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-user_watchlist = {}
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
@@ -22,44 +28,84 @@ async def price(ctx, ticker):
     quote = stonk.get_response(ticker)
     await ctx.send(quote)
 
-@bot.command()
-async def watchlist(ctx, op: str, stock = ''):
-    user = ctx.message.author.id
-    if op == 'add':
-        if user in user_watchlist:
-            if stock in user_watchlist[user]:
-                await ctx.send(f"{stock} is already in your watchlist") 
-            else:
-                user_watchlist[user][stock] = stonk.get_response(stock)
-                await ctx.send(f"adding {stock} to your watchlist")     
-        else:
-            user_watchlist[user] = {stock: stonk.get_response(stock)}
-            await ctx.send(f"adding {stock} to your watchlist")
-    if op == 'remove' or op == 'delete':
-        if user in user_watchlist:
-            if stock in user_watchlist[user]:
-                del user_watchlist[user][stock]
-                await ctx.send(f"removing {stock} from your watchlist")
-            else:
-                await ctx.send(f"{stock} not found in your watchlist")
-        else:
-            await ctx.send("You do not have a watchlist")
-    if op == 'show' or op == 'print' or op == 'view':
-        if user in user_watchlist:
-            message = 'Your watchlist:\n'
-            for s, val in user_watchlist[user].items():
-                message += f'{s} {val}\n'
-            await ctx.send(message)
-        else:
-            await ctx.send("No stocks in watchlist")
-    if op == 'wipe' or op == 'clear':
-        if user in user_watchlist:
-            user_watchlist[user].clear()
-            await ctx.send("Watchlist wiped")
-        else:
-            await ctx.send("You do not have a watchlist")
-        # await ctx.send(f"printing watchlist...")
 
-    
+watchlist_file = "watchlist.json"
+
+def load_watchlist():
+    try:
+        with open(watchlist_file, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {"users": {}}
+
+def save_watchlist(watchlist):
+    with open(watchlist_file, "w") as f:
+        json.dump(watchlist, f)
+
+@bot.command()
+async def addstock(ctx, ticker: str):
+    author_id = str(ctx.message.author.id)
+    watchlist = load_watchlist()
+    ticker = ticker.upper()
+    if author_id not in watchlist["users"]:
+        watchlist["users"][author_id] = {"watchlist": []}
+    if ticker in [s["ticker"] for s in watchlist["users"][author_id]["watchlist"]]:
+        await ctx.send(f"{ticker} already exists in your watchlist.")
+    else:
+        watchlist["users"][author_id]["watchlist"].append({"ticker": ticker})
+        save_watchlist(watchlist)
+        await ctx.send(f"{ticker} has been added to your watchlist.")
+
+@bot.command()
+async def removestock(ctx, ticker: str):
+    author_id = str(ctx.message.author.id)
+    watchlist = load_watchlist()
+    ticker = ticker.upper()
+    if author_id not in watchlist["users"]:
+        await ctx.send("You don't have any stocks in your watchlist.")
+    else:
+        if ticker in [s["ticker"] for s in watchlist["users"][author_id]["watchlist"]]:
+            watchlist["users"][author_id]["watchlist"] = [s for s in watchlist["users"][author_id]["watchlist"] if s["ticker"] != ticker]
+            save_watchlist(watchlist)
+            await ctx.send(f"{ticker} has been removed from your watchlist.")
+        else:
+            await ctx.send(f"{ticker} not found in your watchlist.")
+
+@bot.command()
+async def printwatchlist(ctx):
+    author_id = str(ctx.message.author.id)
+    watchlist = load_watchlist()
+    if author_id not in watchlist["users"]:
+        await ctx.send("You don't have any stocks in your watchlist.")
+    else:
+        stocks = [s["ticker"] for s in watchlist["users"][author_id]["watchlist"]]
+        await ctx.send(f"Your watchlist: {', '.join(stocks)}")
+
+@bot.command()
+async def plotwatchlist(ctx):
+    author_id = str(ctx.message.author.id)
+    watchlist = load_watchlist()
+    user = bot.get_user(ctx.message.author.id)
+    if author_id not in watchlist["users"]:
+        await ctx.send("You don't have any stocks in your watchlist.")
+    else:
+        stocks = [s["ticker"] for s in watchlist["users"][author_id]["watchlist"]]
+        df = yf.download(stocks[0])
+        ax = df.plot(y='Close', label=stocks[0], linewidth=0.85)
+        
+        for stock in stocks:
+            if stock != stocks[0]:
+                df = yf.download(stock)
+                df.plot(ax=ax, y='Close', label=stock, linewidth=0.85)
+        plt.title(f"{user.name}'s Watchlist")
+        plt.xlabel('Date')
+        plt.ylabel('Close')
+        plt.grid(True)
+        plt.savefig("images/watchlist.png")
+ 
+        await ctx.send(file=discord.File("images/watchlist.png"))
+
+        os.remove('images/watchlist.png')
+
 
 bot.run(TOKEN)
